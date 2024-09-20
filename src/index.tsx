90f3ef077cbc35f3ca5336582e361112c8498bc5
EgorUU <egorsairanov12@gmail.com>
2024-09-20 17:51:52 +0300

    second reincornation

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
